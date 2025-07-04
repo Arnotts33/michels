f99b5335efd7b1f995903655545f5921a16715b7
Arnaud Lahaut <arnaudlht@gmail.com>
2025-07-04 23:25:04 +0200

    Implement: Hero section main layout

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,1 +1,18 @@
+import Image from "astro/components/Image.astro";
 import styles from "./Header.module.css";
+
+const Header = () => {
+	return (
+		<header className={styles.header}>
+			<div>
+				<a href="/">
+					<img
+						src="/src/assets/images/logo/michels-logo.svg"
+						alt="Bistrot Michel's Logo"
+					/>
+				</a>
+			</div>
+		</header>
+	);
+};
+export default Header;
