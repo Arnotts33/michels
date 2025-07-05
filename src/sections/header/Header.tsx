9d0f70b4267becf40d74715aa74eaaba6e83aded
Arnaud Lahaut <arnaudlht@gmail.com>
2025-07-05 19:41:35 +0200

    Fix: Image classic  import to work with Vercel

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 			<div className={styles.innerHeader}>
 				<a href="/">
 					<img
-						src="/src/assets/images/logo/michels-logo.svg"
+						src="/assets/images/logo/michels-logo.svg"
 						alt="Bistrot Michel's Logo"
 					/>
 				</a>
