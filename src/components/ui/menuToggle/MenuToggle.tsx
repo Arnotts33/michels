711744438bfd67012e6b84a62782bdf7c86226ef
Arnaud Lahaut <arnaudlht@gmail.com>
2025-07-06 17:15:33 +0200

    Add: menu toggle icon animation and logic with state

--- src/components/ui/menuToggle/MenuToggle.tsx
+++ src/components/ui/menuToggle/MenuToggle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./MenuToggle.module.css";
 
 type MenuToggleProps = {
@@ -6,9 +7,11 @@ type MenuToggleProps = {
 };
 
 const MenuToggle = ({ category }: MenuToggleProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<div className={styles.menuCardWrapper}>
-			<div className={styles.menuCard}>
+			<div className={styles.menuCard} onClick={() => setIsOpen(!isOpen)}>
 				<h3 className={styles.menuTitle}>{category}</h3>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +23,9 @@ const MenuToggle = ({ category }: MenuToggleProps) => {
 					stroke-width="3"
 					stroke-linecap="round"
 					stroke-linejoin="round"
-					className={styles.menuToggleIcon}
+					className={`${styles.menuToggleIcon} ${
+						isOpen ? styles.menuToggleClose : ""
+					}`}
 				>
 					<path d="M5 12h14" />
 					<path d="M12 5v14" />
