47d934062f68b55a496d1beb9a24441e02ef876d
Arnaud Lahaut <arnaudlht@gmail.com>
2025-07-07 18:35:38 +0200

    Add:responsive  auto scrolling galery

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -11,15 +11,15 @@ export const menuData = [
 		category: "Les Classiques",
 		items: [
 			{ name: "Confit de canard", side: "Purée maison", price: "17,90 €" },
-			{ name: "Croque Monsieur", side: "Purée maison", price: "12,90 €" },
+			{ name: "Croque Monsieur", side: "Frites fraîches", price: "12,90 €" },
 			{
 				name: "Tartare de bœuf",
-				side: "Purée maison",
+				side: "Au couteau, frites fraîches",
 				price: "18,90 €",
 			},
 			{
 				name: "Saucisse de porc",
-				side: "Purée maison",
+				side: "Porc Duroc, purée maison",
 				price: "13,90 €",
 			},
 		],
@@ -28,11 +28,11 @@ export const menuData = [
 		category: "Petites Assiettes",
 		items: [
 			{ name: "Oeufs bio mimosa", price: "6,90 €" },
-			{ name: "Bulots", side: "Purée maison", price: "6,90 €" },
+			{ name: "Bulots", side: "Mayonnaise maison", price: "6,90 €" },
 		],
 	},
 	{
-		category: "Charcuterie & Fromages",
+		category: "Charcuterie & Fromage",
 		items: [
 			{ name: "Oeufs bio mimosa", price: "6,90 €" },
 			{ name: "Bulots", side: "Purée maison", price: "6,90 €" },
