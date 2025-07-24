08e3ef905e383fd872360c6cfe6ffd20a6eb9496
Arnaud Lahaut <arnaudlht@gmail.com>
2025-07-25 00:21:07 +0200

    Add: SEO configuration

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,10 +4,16 @@ const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
 
+Disallow: /assets/fonts/
+
 Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
 	const sitemapURL = new URL("sitemap-index.xml", site);
-	return new Response(getRobotsTxt(sitemapURL));
+	return new Response(getRobotsTxt(sitemapURL), {
+		headers: {
+			"Content-Type": "text/plain; charset=utf-8",
+		},
+	});
 };
